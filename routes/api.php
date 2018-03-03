503a89c27b59a7c9d151927b82473797931f7bef
Diaa Zaiter <dmz12@mail.aub.edu>
2018-03-03 20:27:54 +0200

    test with db

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::group(['middleware' => 'cors', 'prefix' => '/v1'], function () {
+    Route::post('/login', 'UserController@authenticate');
+    Route::post('/register', 'UserController@register');
+    Route::post('/logout/{api_token}', 'UserController@logout');
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
